e8f1bf3038d4f739cdcb71a8eb464dd1a61d075c
viswatejavissamsetty <viswateja994@gmail.com>
2022-01-16 13:18:17 +0530

    test

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log(payload);
-    return { idCardNumber: payload._id, password: payload.username };
+    return { idCardNumber: payload.name, sub: payload.sub };
   }
 }
