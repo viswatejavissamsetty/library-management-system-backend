79f8258b1573dc56763ffb7b506d1b482083291f
viswatejavissamsetty <viswateja994@gmail.com>
2022-01-16 13:22:52 +0530

    ggsdfgds

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,21 +6,9 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('new-user')
-<<<<<<< HEAD
-  async createUser(@Body() userData: User) {
-    userData._id = userData.idCardNumber;
-    try {
-      const user = await this.usersService.create(userData);
-      const { password, _id, ...responseData } = user.toJSON();
-      return responseData;
-    } catch (error) {
-      throw new HttpException(error.message, error.status);
-    }
-=======
   createUser(@Body() userData: User) {
     console.log(userData);
 
     this.usersService.create(userData);
->>>>>>> parent of 944f77a (authentication done)
   }
 }
