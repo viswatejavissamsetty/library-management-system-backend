e571c63d870332908eaff33b94b2fcd7e21f877e
viswatejavissamsetty <viswateja994@gmail.com>
2022-01-19 15:34:19 +0530

    public folder creation

--- src/book-orders/book-orders.controller.ts
+++ src/book-orders/book-orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { BookOrder, BookOrdersService } from './book-orders.service';
 
@@ -8,8 +8,57 @@ export class BookOrdersController {
 
   @UseGuards(JwtAuthGuard)
   @Post('/new-request')
-  async function(@Body() newBookRequest: BookOrder) {
-    // console.log(newBookRequest);
+  async takeNewBook(@Body() newBookRequest: BookOrder) {
     return this.bookOrderService.planToTakeBook(newBookRequest);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/getAllUserPlannedBooks')
+  async getAllUserPlannedBooks(@Query('userId') userId: string) {
+    return this.bookOrderService.getAllUserPlannedBooks(userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/getAllPlannedBooks')
+  async getAllPlannedBooks(@Query('userId') userId: string) {
+    return this.bookOrderService.getAllPlannedBooks(userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('getAllUserTakenBooks')
+  async getAllUserTakenBooks(@Query('userId') userId: string) {
+    return this.bookOrderService.getAllUserTakenBooks(userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('getAllTakenBooks')
+  async getAllTakenBooks(@Query('userId') userId: string) {
+    return this.bookOrderService.getAllTakenBooks(userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('takeBook')
+  async takeBooks(
+    @Body() payload: { librarianId: string; trackingId: string },
+  ) {
+    return this.bookOrderService.takeABook(
+      payload.librarianId,
+      payload.trackingId,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('returnBook')
+  async returnBook(@Body() tackingData: { id: string; librarianId: string }) {
+    return this.bookOrderService.returnBook(
+      tackingData.id,
+      tackingData.librarianId,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('cancelBook')
+  async cancelBook(@Body() tackingData: { id: string }) {
+    return this.bookOrderService.cancelBook(tackingData.id);
   }
 }
--- src/book-orders/book-orders.service.ts
+++ src/book-orders/book-orders.service.ts
@@ -5,7 +5,7 @@ import { BooksService } from 'src/books/books.service';
 import { CreateBookOrderDto as BookOrderDto } from 'src/types/create-book-order';
 import { UsersService } from 'src/users/users.service';
 
-const checkTime = 60 * 60 * 24 * 2; // sec * min * hours * days
+const checkTime = 60 * 60 * 24 * 2; // sec * min * hours * days -> here calculation indicates that is was 2 days.
 
 export type BookOrder = {
   readonly _id: string;
@@ -27,16 +27,38 @@ export class BookOrdersService {
     private bookService: BooksService,
   ) {}
 
-  async getAllPlannedBooks(): Promise<BookOrderDto[]> {
-    return this.bookOrderModel.find({ status: 'PLANNED' });
+  async getAllPlannedBooks(id: string): Promise<BookOrderDto[]> {
+    const user = await this.userService.findOne(id);
+    if (user.isLibrarian) {
+      return this.bookOrderModel.find({ status: 'PLANNED' });
+    } else {
+      throw new HttpException('Invalid User', HttpStatus.FORBIDDEN);
+    }
   }
 
-  async getAllTakenBooks(): Promise<BookOrderDto[]> {
-    return this.bookOrderModel.find({ status: 'TAKEN' });
+  async getAllUserPlannedBooks(userId: string): Promise<BookOrderDto[]> {
+    return this.bookOrderModel.find({ userId, status: 'PLANNED' });
+  }
+
+  async getAllTakenBooks(id: string): Promise<BookOrderDto[]> {
+    const user = await this.userService.findOne(id);
+    if (user.isLibrarian) {
+      return this.bookOrderModel.find({ status: 'TAKEN' });
+    } else {
+      throw new HttpException('Invalid User', HttpStatus.FORBIDDEN);
+    }
+  }
+
+  async getAllUserTakenBooks(userId: string): Promise<BookOrderDto[]> {
+    return this.bookOrderModel.find({ userId, status: 'TAKEN' });
   }
 
   async getAllReturnedBooks(): Promise<BookOrderDto[]> {
     return this.bookOrderModel.find({ status: 'RETURNED' });
+  }
+
+  async getAllUserReturnedBooks(userId: string): Promise<BookOrderDto[]> {
+    return this.bookOrderModel.find({ userId, status: 'RETURNED' });
   }
 
   async planToTakeBook(newData: BookOrder) {
@@ -53,18 +75,22 @@ export class BookOrdersService {
   }
 
   async returnBook(
-    userId: string,
-    bookId: string,
-    issuer: string,
+    // userId: string,
+    // bookId: string,
+    // issuer: string,
+    trackingId: string,
+    librarianId: string,
   ): Promise<UpdateWriteOpResult> {
-    const issuerDetails = await this.userService.findOne(issuer);
+    const issuerDetails = await this.userService.findOne(librarianId);
+    const bookDetails = await this.bookOrderModel.findById(trackingId);
     if (issuerDetails.isLibrarian) {
       const data = await this.bookOrderModel.updateOne(
-        { userId, bookId, issuer },
+        // { userId, bookId, issuer },
+        { _id: trackingId },
         { status: 'RETURNED' },
       );
       if (data) {
-        await this.bookService.updateBookCount(bookId, 1);
+        await this.bookService.updateBookCount(bookDetails.bookId, 1);
         return data;
       }
     } else {
@@ -72,32 +98,56 @@ export class BookOrdersService {
     }
   }
 
-  async cancelBook(bookData: BookOrder) {
-    bookData.planedDate = new Date();
-    bookData.takenDate = '';
-    bookData.returnedDate = '';
-    bookData.status = 'CANCELLED';
-    await this.bookService.updateBookCount(bookData.bookId, 1);
+  async takeABook(librarianId: string, tracingId: string) {
+    const user = await this.userService.findOne(librarianId);
+    if (user && user.isLibrarian) {
+      const date = new Date();
+      date.setDate(date.getDate() + 15);
+      return this.bookOrderModel.updateOne(
+        { _id: tracingId },
+        {
+          takenDate: new Date(),
+          returnedDate: date,
+          status: 'TAKEN',
+          issuer: librarianId,
+        },
+      );
+    } else {
+      throw new HttpException('Invalid User', HttpStatus.FORBIDDEN);
+    }
+  }
+
+  // async cancelBook(bookData: BookOrder) {
+  async cancelBook(id: string) {
+    // bookData.planedDate = new Date();
+    // bookData.takenDate = '';
+    // bookData.returnedDate = '';
+    // bookData.status = 'CANCELLED';
+    const bookDetails = await this.bookOrderModel.findById(id);
+    await this.bookService.updateBookCount(bookDetails.bookId, 1);
     const cancelBookStatus = await this.bookOrderModel.updateOne(
       {
-        bookId: bookData.bookId,
-        userId: bookData.userId,
-        status: 'PLANNED',
+        _id: id,
       },
       { status: 'CANCELLED' },
     );
     return cancelBookStatus;
   }
 
+  private async getAllUserPlannedBooksCronProcess(): Promise<BookOrderDto[]> {
+    return this.bookOrderModel.find({ status: 'PLANNED' });
+  }
+
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handleCron() {
     console.log('Runnning Cron Job');
-    const plannedBooks = await this.getAllPlannedBooks();
+    const plannedBooks = await this.getAllUserPlannedBooksCronProcess();
     plannedBooks.forEach((book) => {
       const date1 = new Date(book.planedDate);
       const date2 = new Date();
       if (date2.getTime() - date1.getTime() >= checkTime) {
-        this.cancelBook(book);
+        // this.cancelBook(book);
+        this.cancelBook(book._id);
       }
     });
   }
