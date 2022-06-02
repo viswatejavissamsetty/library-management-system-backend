d95ada4663a48cf92810d6082e1cb793fe270879
VISWA <63993225+viswatejavissamsetty@users.noreply.github.com>
2022-06-02 10:33:19 +0530

    changesd port

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(3000, () => {
+  await app.listen(15300, () => {
     console.log(`Server is started`);
   });
 }
